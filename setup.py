135506f2dfede32d5337ea0e8b5939c1e1dd7888
sehar <boricisehar@gmail.com>
2023-11-14 00:15:02 +0100

    'fixes'

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
+    name='tiktok_downloader',
     version='0.1',
     packages=find_packages(),
     install_requires=[
